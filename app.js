a84b8ddcb7a37c682fdcec108b1870788eb08d95
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-01-14 18:49:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (name === '' || email === '' || password === '' || confirmPassword === '') {
+        alert('Please fill all the fields');
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match');
+        return;
+    }
+
+    alert('Form Submitted');
+});
